715f553e1e7bd0802d318c36f454b35c5e266a42
Bhargav200004 <Bhargav200004@gmail.com>
2024-06-21 16:28:47 +0530

    Editing Readme file

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,11 +22,11 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-network-tls-certificates:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-websockets-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-network-tls-certificates:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
